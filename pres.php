53eb7177adf1b3c42b74618eef6f6eee16074ed6
isivroag <isivroag@gmail.com>
2020-09-23 14:49:09 -0500

    cambios buton preview

--- pres.php
+++ pres.php
@@ -111,7 +111,7 @@ else{
 
             <button id="btnNuevo" type="button" class="btn bg-gradient-orange btn-ms" data-toggle="modal"><i class="fas fa-plus-square"></i> Nuevo</button>
             <button type="button" id="btnGuardar" name="btnGuardar" class="btn btn-success" value="btnGuardar"><i class="far fa-save"></i> Guardar</button>
-            <button id="btnVer" name="btnVer" type="button" class="btn bg-gradient-primary btn-ms"><i class="fas fa-file-pdf"></i> Enviar</button>
+            <button id="btnVer" name="btnVer" type="button" class="btn bg-gradient-primary btn-ms"><i class="fas fa-file-pdf"></i> Preview</button>
             <button id="btnEnviar" name="btnEnviar" type="button" class="btn bg-gradient-purple btn-ms"><i class="fas fa-envelope-square"></i> Enviar</button>
           </div>
         </div>
