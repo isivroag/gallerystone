e1d4afc02b23083df30cece2984560d2ffa438c5
isivroag <isivroag@gmail.com>
2022-01-27 23:56:38 -0600

    ACT 27ENE22

--- templates/navegacion.php
+++ templates/navegacion.php
@@ -23,6 +23,7 @@
     </div>
 
     <!-- Sidebar Menu -->
+    
     <nav class="mt-2">
       <ul class="nav nav-pills nav-sidebar nav-compact nav-child-indent flex-column" data-widget="treeview" role="menu" data-accordion="false">
         <!-- Add icons to the links using the .nav-icon class
